6bcf714c2b1a5adca6254472310a69940903da27
lr-2026 <lrmassas@syr.edu>
2026-09-01 19:16:42 +0000

    summary type and models

--- Lab1.py
+++ Lab1.py
@@ -2,6 +2,7 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
+
 st.title("My Document question answering")
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
@@ -10,6 +11,29 @@ st.write(
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+
+options = ["Nano", "Mini", "Advanced Model"]
+model = st.sidebar.radio("Models", options, index = None)
+
+summary_type = st.sidebar.selectbox(
+    'Summary Type',
+    ('Summarize in 100 words',
+     'Summarize in 2 connecting paragraphs',
+     'Summarize in 5 bullet points'
+)
+)
+
+if model == "Nano":
+    model_choice = "gpt-5-nano"
+elif model == "Mini":
+    model_choice = "gpt-5-mini"
+elif model == "Advanced Model":
+    model_choice = "gpt-5"
+else:
+    model_choice = None
+
+
+
 openai_api_key = st.secrets["My_newkey"]
 
 
@@ -21,27 +45,20 @@ uploaded_file = st.file_uploader(
     "Upload a document (.txt or .md)", type=("txt", "md")
 )
 
-# Ask the user for a question via `st.text_area`.
-question = st.text_area(
-    "Now ask a question about the document!",
-    placeholder="Can you give me a short summary?",
-    disabled=not uploaded_file,
-)
+if uploaded_file and summary_type:
 
-if uploaded_file and question:
-
-    # Process the uploaded file and question.
+    # Process the uploaded file and summary type.
     document = uploaded_file.read().decode()
     messages = [
         {
             "role": "user",
-            "content": f"Here's a document: {document} \n\n---\n\n {question}",
+            "content": f"Here's a document: {document} \n\n---\n\n {summary_type}",
         }
     ]
 
     # Generate an answer using the OpenAI API.
     stream = client.chat.completions.create(
-        model="gpt-5-nano",
+        model=model_choice,
         messages=messages,
         stream=True,
     )
--- Lab2.py
+++ Lab2.py
@@ -4,6 +4,7 @@ from openai import OpenAI
 
 # Show title and description.
 st.title("Lab 2 Document ")
+
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! " 
 )
@@ -11,7 +12,32 @@ st.write(
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+
+options = ["Nano", "Mini", "Advanced Model"]
+model = st.sidebar.radio("Models", options, index = None)
+
+summary_type = st.sidebar.selectbox(
+    'Summary Type',
+    ('Summarize in 100 words',
+     'Summarize in 2 connecting paragraphs',
+     'Summarize in 5 bullet points'
+)
+)
+
+
+if model == "Nano":
+    model_choice = "gpt-5-nano"
+elif model == "Mini":
+    model_choice = "gpt-5-mini"
+elif model == "Advanced Model":
+    model_choice = "gpt-5"
+else:
+    model_choice = None
+
+
+
 openai_api_key = st.secrets["My_newkey"]
+
 
 # Create an OpenAI client.
 client = OpenAI(api_key=openai_api_key)
@@ -21,27 +47,20 @@ uploaded_file = st.file_uploader(
     "Upload a document (.txt or .md)", type=("txt", "md")
 )
 
-# Ask the user for a question via `st.text_area`.
-question = st.text_area(
-    "Now ask a question about the document!",
-    placeholder="Can you give me a short summary?",
-    disabled=not uploaded_file,
-)
+if uploaded_file and summary_type:
 
-if uploaded_file and question:
-
-    # Process the uploaded file and question.
+    # Process the uploaded file and summary type.
     document = uploaded_file.read().decode()
     messages = [
         {
             "role": "user",
-            "content": f"Here's a document: {document} \n\n---\n\n {question}",
+            "content": f"Here's a document: {document} \n\n---\n\n {summary_type}",
         }
     ]
 
     # Generate an answer using the OpenAI API.
     stream = client.chat.completions.create(
-        model="gpt-5-nano",
+        model=model_choice,
         messages=messages,
         stream=True,
     )
